4807517484d135a23919fa9598735ffda1d2a185
Tyrantz <raymanv3@gmail.com>
2016-03-26 15:13:01 +0000

    adds markdown render functionality

--- src/app.js
+++ src/app.js
@@ -1,22 +1,51 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import marked from 'marked';
+require("./style.scss");
+
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
 
 var App = React.createClass({
+  getInitialState() {
+    return {raw_markup: ""};
+  },
+  changeHandler(value) {
+    this.setState({
+      raw_markup: value
+    });
+  },
   render() {
     return (
-      <div>
-        <InputBox />
-        <OutputBox />
+      <div className="app-container">
+        <InputBox changeHandler={this.changeHandler}/>
+        <OutputBox inputedMarkup={this.state.raw_markup}/>
       </div>
     );
   }
 });
 
 var InputBox = React.createClass({
+  getInitialState() {
+    return {value: 'Type your Github Markdown here!'};
+  },
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    this.props.changeHandler(event.target.value);
+  },
   render() {
     return (
       <div className="input-box">
-        <textarea id="markup-input"></textarea>
+        <textarea id="markup-input" value={this.state.value} onChange={this.handleChange}></textarea>
       </div>
     );
   }
@@ -25,7 +54,7 @@ var InputBox = React.createClass({
 var OutputBox = React.createClass({
   render() {
     return (
-      <div className="output-box" id="markup-output">I am an output box!</div>
+      <div className="output-box" id="markup-output" dangerouslySetInnerHTML={{ __html: marked(this.props.inputedMarkup) }}></div>
     );
   }
 });
